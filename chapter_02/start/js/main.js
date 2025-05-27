5e1766f2b9b93af1e146945227e9818627d46591
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-05-27 16:11:50 +0200

    Apply style with d3js

--- chapter_02/start/js/main.js
+++ chapter_02/start/js/main.js
@@ -1,6 +1,7 @@
 const svg = d3.select(".responsive-svg-container")
   .append("svg")
-    .attr("viewBox", "0 0 1200 1600");
+    .attr("viewBox", "0 0 1200 1600")
+    .style("border", "1px solid gray");
 
 const h = 16;
 
