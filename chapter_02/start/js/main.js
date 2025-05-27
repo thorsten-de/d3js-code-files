9df601098ddd24e01a8aa239532865df097a2a9a
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-05-27 16:20:05 +0200

    Clear project for next steps

--- chapter_02/start/js/main.js
+++ chapter_02/start/js/main.js
@@ -2,14 +2,3 @@ const svg = d3.select(".responsive-svg-container")
   .append("svg")
     .attr("viewBox", "0 0 1200 1600")
     .style("border", "1px solid gray");
-
-const h = 16;
-
-svg
-  .append("rect")
-    .attr("x", 10)
-    .attr("y", 10)
-    .attr("width", 414)
-    .attr("height", h)
-    .attr("fill", "turquoise")
-    .style("fill", "plum");
