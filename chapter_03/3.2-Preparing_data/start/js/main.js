7d14e72a758c2a72d9aa2b112c81f0d6427a828d
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-01 17:24:21 +0200

    Load, Format and Measure data

--- chapter_03/3.2-Preparing_data/start/js/main.js
+++ chapter_03/3.2-Preparing_data/start/js/main.js
@@ -3,3 +3,23 @@ const svg = d3.select(".responsive-svg-container")
   .append("svg")
   .attr("viewBox", "0 0 1200 1600")
   .style("border", "1px solid black");
+
+d3.csv("../data/data.csv", row => {
+  // use callback for each row to format data, here counts to integer
+  return {
+    tech: row.technology,
+    count: +row.count
+  }
+}).then(data => {
+  // this is a promise, so handle it after it is fulfilled
+  console.log(data.length);
+  console.log(d3.extent(data, d => d.count));
+  //data.sort((a, b) => b.count - a.count);
+  data = d3.sort(data, d => -d.count);
+
+  createViz(data);
+});
+
+const createViz = data => {
+  console.log(data)
+};
