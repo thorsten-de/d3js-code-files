c0224201421321ad10524dc95438018f25ef46bd
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-03 17:30:59 +0200

    Add labels for tech and counts
    - group each data row
    - add a vertical line
    - remove border

--- chapter_03/3.2-Preparing_data/start/js/main.js
+++ chapter_03/3.2-Preparing_data/start/js/main.js
@@ -2,7 +2,6 @@
 const svg = d3.select(".responsive-svg-container")
   .append("svg")
   .attr("viewBox", "0 0 600 700")
-  .style("border", "1px solid black");
 
 d3.csv("../data/data.csv", row => {
   // use callback for each row to format data, here counts to integer
@@ -31,14 +30,42 @@ const createViz = data => {
     .range([0, 700])
     .paddingInner(0.2);
 
-  svg
-    .selectAll("rect")
+  const barAndLabel = svg
+    .selectAll("g")
     .data(data)
-    .join("rect")
-    .attr("class", "bar")
+    .join("g")
+    .attr("transform", d => `translate(0, ${yScale(d.tech)})`);
+
+  barAndLabel
+    .append("rect")
     .attr("width", d => xScale(d.count))
     .attr("height", yScale.bandwidth())
     .attr("x", 100)
-    .attr("y", d => yScale(d.tech))
+    .attr("y", 0)
     .attr("fill", d => d.tech === "D3.js" ? "steelblue" : "lightsteelblue")
+
+  barAndLabel
+    .append("text")
+    .text(d => d.tech)
+    .attr("x", 96)
+    .attr("y", 12)
+    .attr("text-anchor", "end")
+    .style("font-family", "sans-serif")
+    .style("font-size", "11px");
+
+  barAndLabel
+    .append("text")
+    .text(d => d.count)
+    .attr("x", d => 100 + xScale(d.count) + 4)
+    .attr("y", 12)
+    .style("font-family", "sans-serif")
+    .style("font-size", "9px");
+
+  svg
+    .append("line")
+    .attr("x1", 100)
+    .attr("y1", 0)
+    .attr("x2", 100)
+    .attr("y2", 700)
+    .attr("stroke", "black");
 }
