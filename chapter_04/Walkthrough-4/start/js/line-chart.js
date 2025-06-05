60fbac97835b21ec3d5b7ad21a5e765ec942ce3a
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-05 20:16:32 +0200

    Load data for temperature viz

--- chapter_04/Walkthrough-4/start/js/line-chart.js
+++ chapter_04/Walkthrough-4/start/js/line-chart.js
@@ -1,6 +1,9 @@
 // Load the data here
+d3.csv("../data/weekly_temperature.csv", d3.autoType)
+  .then(d => drawLineChart(d));
 
 // Create the line chart here
 const drawLineChart = (data) => {
+  console.log("Temperature data", data)
 
 };
