48a841a7b32b559a6a8c383498b926d8ad368cfa
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-05 20:26:05 +0200

    Use  margin convention

--- chapter_04/Walkthrough-4/start/js/line-chart.js
+++ chapter_04/Walkthrough-4/start/js/line-chart.js
@@ -4,6 +4,17 @@ d3.csv("../data/weekly_temperature.csv", d3.autoType)
 
 // Create the line chart here
 const drawLineChart = (data) => {
-  console.log("Temperature data", data)
+  const margin = { top: 40, right: 170, bottom: 25, left: 40 };
+  const width = 600;
+  const height = 400;
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
 
+  const svg = d3.select("#line-chart")
+    .append("svg")
+    .attr("viewBox", `0 0 ${width} ${height}`);
+
+  const innerChart = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 };
