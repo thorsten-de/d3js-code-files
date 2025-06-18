cce3c2b1e428f0a7a929c074617bfd287e2b7cf6
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-18 16:03:53 +0200

    Append svg container for donut charts area

--- chapter_05/5.1-Pie_layout/start/js/donut-charts.js
+++ chapter_05/5.1-Pie_layout/start/js/donut-charts.js
@@ -1,5 +1,10 @@
 const drawDonutCharts = (data) => {
-  // Generate the donut charts here
+  const svg = d3.select("#donut")
+    .append("svg")
+    .attr("viewBox", `0 0 ${width} ${height}`);
 
-  
+  const donutContainers = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
 };
