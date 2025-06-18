36854e564389cd4f2c6ed513534e617b4b80f9fd
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-18 16:42:49 +0200

    Add labels on arc centroids

--- chapter_05/5.1-Pie_layout/start/js/donut-charts.js
+++ chapter_05/5.1-Pie_layout/start/js/donut-charts.js
@@ -11,15 +11,49 @@ const drawDonutCharts = (data) => {
         sales: yearData[format]
       };
     });
+
+    const pieGenerator = d3.pie()
+      .value(d => d.sales);
     const annotatedData = pieGenerator(formattedData);
+
 
     const arcs = donutContainer
       .selectAll(`.arc-${year}`)
       .data(annotatedData)
-      .join("path")
-      .attr("class", `arc-${year}`)
+      .join("g")
+      .attr("class", `arc-${year}`);
+
+    const arcGenerator = d3.arc()
+      .startAngle(d => d.startAngle)
+      .endAngle(d => d.endAngle)
+      .innerRadius(60)
+      .outerRadius(100)
+      .padAngle(0.02)
+      .cornerRadius(3);
+
+    arcs.append("path")
       .attr("d", arcGenerator)
       .attr("fill", d => colorScale(d.data.format));
+
+    arcs.append("text")
+      .text(d => {
+        d["percentage"] = (d.endAngle - d.startAngle) / (2 * Math.PI);
+        return d3.format(".0%")(d.percentage);
+      })
+      .attr("x", d => {
+        d["centroid"] = arcGenerator
+          .startAngle(d.startAngle)
+          .endAngle(d.endAngle)
+          .centroid();
+        return d.centroid[0];
+      })
+      .attr("y", d => d.centroid[1])
+      .attr("text-anchor", "middle")
+      .attr("dominant-baseline", "middle")
+      .attr("fill", "#f6fafc")
+      .attr("fill-opacity", d => d.percentage < 0.05 ? 0 : 1)
+      .style("font-size", "16px")
+      .style("font-weight", 500);
   }
 
   const svg = d3.select("#donut")
@@ -33,16 +67,7 @@ const drawDonutCharts = (data) => {
   const years = [1975, 1995, 2013];
   const formats = data.columns.filter(format => format !== "year");
 
-  const pieGenerator = d3.pie()
-    .value(d => d.sales);
 
-  const arcGenerator = d3.arc()
-    .startAngle(d => d.startAngle)
-    .endAngle(d => d.endAngle)
-    .innerRadius(60)
-    .outerRadius(100)
-    .padAngle(0.02)
-    .cornerRadius(3);
 
   years.forEach(drawYearDonut);
 };
