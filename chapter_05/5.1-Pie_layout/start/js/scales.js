13faf22c67b0198040d5a542f89435da2cd379a0
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-18 16:05:31 +0200

    Define global band scale for horizontally spacing the years

--- chapter_05/5.1-Pie_layout/start/js/scales.js
+++ chapter_05/5.1-Pie_layout/start/js/scales.js
@@ -1,8 +1,10 @@
 // Initialize the scales here
+const xScale = d3.scaleBand();
 
- 
+
 const defineScales = (data) => {
   // Define the scales domain and range here
-
-  
+  xScale
+    .domain(data.map(d => d.year))
+    .range([0, innerWidth])
 };
