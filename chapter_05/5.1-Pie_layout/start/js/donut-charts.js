bb5582edebf1bb076d5d28d9402264658d9830ab
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-18 16:15:18 +0200

    Refactor with local function

--- chapter_05/5.1-Pie_layout/start/js/donut-charts.js
+++ chapter_05/5.1-Pie_layout/start/js/donut-charts.js
@@ -1,15 +1,5 @@
 const drawDonutCharts = (data) => {
-  const svg = d3.select("#donut")
-    .append("svg")
-    .attr("viewBox", `0 0 ${width} ${height}`);
-
-  const donutContainers = svg
-    .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-  const years = [1975, 1995, 2013];
-  const formats = data.columns.filter(format => format !== "year");
-  years.forEach(year => {
+  const drawYearDonut = year => {
     const donutContainer = donutContainers
       .append("g")
       .attr("transform", `translate(${xScale(year)}, ${innerHeight / 2})`)
@@ -21,6 +11,17 @@ const drawDonutCharts = (data) => {
         sales: yearData[format]
       };
     });
+  }
 
-  });
+  const svg = d3.select("#donut")
+    .append("svg")
+    .attr("viewBox", `0 0 ${width} ${height}`);
+
+  const donutContainers = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  const years = [1975, 1995, 2013];
+  const formats = data.columns.filter(format => format !== "year");
+  years.forEach(drawYearDonut);
 };
