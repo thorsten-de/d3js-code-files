de3921635d6b5db72c39347681a505ac21fb8828
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-20 10:36:19 +0200

    Use stack layout generator to transform data

--- chapter_05/5.1-Pie_layout/start/js/stacked-bars.js
+++ chapter_05/5.1-Pie_layout/start/js/stacked-bars.js
@@ -1,17 +1,23 @@
 const drawStackedBars = (data) => {
   // Generate the stacked bar chart here
-  
+
 
   /*******************************/
   /*    Append the containers    */
   /*******************************/
   const svg = d3.select("#bars")
-  .append("svg")
+    .append("svg")
     .attr("viewBox", [0, 0, width, height]);
 
   const innerChart = svg
     .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  
+  const stackGenerator = d3.stack()
+    .keys(formatsInfo.map(f => f.id));
+
+  const annotatedData = stackGenerator(data);
+
+  console.log(annotatedData);
+
 };
