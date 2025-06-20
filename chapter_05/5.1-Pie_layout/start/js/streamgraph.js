51e2205df927bbf4f76995451dfd0e19aebfe1e4
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-20 10:55:00 +0200

    Copy stack layout generator from stackbar

--- chapter_05/5.1-Pie_layout/start/js/streamgraph.js
+++ chapter_05/5.1-Pie_layout/start/js/streamgraph.js
@@ -1,17 +1,29 @@
 const drawStreamGraph = (data) => {
   // Generate the streamgraph here
-  
+
 
   /*******************************/
   /*    Append the containers    */
   /*******************************/
   const svg = d3.select("#streamgraph")
-  .append("svg")
+    .append("svg")
     .attr("viewBox", [0, 0, width, height]);
 
   const innerChart = svg
     .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top})`);
-  
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  const stackGenerator = d3.stack()
+    .keys(formatsInfo.map(f => f.id));
+
+  const annotatedData = stackGenerator(data);
+
+  // the last series is stacked at the top, so get the max y1 from there
+  const maxUpperBoundary = d3.max(annotatedData[annotatedData.length - 1], d => d[1]);
+
+  const yScale = d3.scaleLinear()
+    .domain([0, maxUpperBoundary])
+    .range([innerHeight, 0])
+    .nice();
 
 };
