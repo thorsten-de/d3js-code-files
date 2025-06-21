416e0e9937f0501f93b8c84d5e4d5686a678c884
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-21 19:54:56 +0200

    Create a tooltip

--- chapter_07/7.3.1-Simple_tooltip/start/js/interactions.js
+++ chapter_07/7.3.1-Simple_tooltip/start/js/interactions.js
@@ -1,11 +1,28 @@
 const createTooltip = () => {
+  const tooltip = innerChart.append("g")
+    .attr("class", "tooltip")
 
-  // Create the tooltip here
+  tooltip.append("rect")
+    .attr("width", tooltipWidth)
+    .attr("height", tooltipHeight)
+    .attr("rx", 3)
+    .attr("ry", 3)
+    .attr("fill", aubergine)
+    .attr("fill-opacity", 0.75)
+  tooltip.append("text")
+    .text("0.00°F")
+    .attr("x", tooltipWidth / 2)
+    .attr("y", tooltipHeight / 2)
+    .attr("dominant-baseline", "middle")
+    .attr("text-anchor", "middle")
+    .attr("fill", "white")
+    .style("font-weight", 900)
+
 
 }
 
 const handleMouseEvents = () => {
 
   // Handle mouse events here
-  
+
 }
