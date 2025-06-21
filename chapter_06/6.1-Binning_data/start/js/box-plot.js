fb96734a710397e892f6631b05881722b4e526d3
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-21 15:28:47 +0200

    Calculate quantiles for salary distributions

--- chapter_06/6.1-Binning_data/start/js/box-plot.js
+++ chapter_06/6.1-Binning_data/start/js/box-plot.js
@@ -3,7 +3,7 @@ const drawBoxplot = (data) => {
   /*******************************/
   /*    Declare the constants    */
   /*******************************/
-  const margin = {top: 40, right: 30, bottom: 25, left: 60};
+  const margin = { top: 40, right: 30, bottom: 25, left: 60 };
   const width = 555;
   const height = 500;
   const innerWidth = width - margin.left - margin.right;
@@ -16,12 +16,37 @@ const drawBoxplot = (data) => {
   // Append the SVG container
   const svg = d3.select("#boxplot")
     .append("svg")
-      .attr("viewBox", `0, 0, ${width}, ${height}`);
+    .attr("viewBox", `0, 0, ${width}, ${height}`);
 
   // Append the group that will contain the inner chart
   const innerChart = svg
     .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  
+  const genders = ["Female", "Male"];
+
+
+  genders.forEach(gender => {
+    const salaries = data
+      .filter(d => d.gender === gender)
+      .map(d => d.salary);
+
+    const quartileScale = d3.scaleQuantile()
+      .domain(salaries)
+      .range([0, 1, 2, 3]);
+
+    const quartiles = quartileScale.quantiles();
+    const median = d3.median(salaries)
+    const mean = d3.mean(salaries)
+    const [min, max] = d3.extent(salaries)
+
+    console.log({ gender, quartiles, median, mean, min, max })
+
+
+
+  });
+
+
+
+
 };
