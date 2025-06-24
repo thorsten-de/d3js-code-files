afe5391a07346415874debb881188a640df6b910
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-24 13:03:32 +0200

    Append vertical tooltip line and year

--- chapter_07/7.3.2-Compound_tooltip/start/js/interactions.js
+++ chapter_07/7.3.2-Compound_tooltip/start/js/interactions.js
@@ -6,6 +6,30 @@ const createTooltip = (data) => {
   const textLineHeight = 22;
 
   // Create the tooltip here
+  const tooltip = innerChart.append("g")
+    .attr("class", "tooltip");
+
+  const xPos = 0;
+  tooltip.append("line")
+    .attr("x1", xPos)
+    .attr("y1", -30)
+    .attr("x2", xPos)
+    .attr("y2", innerHeight)
+    .attr("stroke-width", 2)
+    .attr("stroke", textColor)
+    .attr("stroke-dasharray", "6 4");
+
+
+  const year = d3.min(data, d => d.year);
+  tooltip.append("text")
+    .text(year)
+    .attr("class", "tooltip-year")
+    .attr("x", xPos)
+    .attr("y", innerHeight + 25)
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px")
+    .style("font-weight", 700)
+    .style("fill", textColor);
 
 };
 
