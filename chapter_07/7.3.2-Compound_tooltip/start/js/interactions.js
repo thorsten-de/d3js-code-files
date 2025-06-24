f5a110457bd651e4465fdcf36914c763cdb86bf6
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-24 13:14:33 +0200

    Append the sales breakdown for first year

--- chapter_07/7.3.2-Compound_tooltip/start/js/interactions.js
+++ chapter_07/7.3.2-Compound_tooltip/start/js/interactions.js
@@ -31,6 +31,30 @@ const createTooltip = (data) => {
     .style("font-weight", 700)
     .style("fill", textColor);
 
+  const tooltipContent = tooltip.append("g")
+    .attr("transform", `translate(${-tooltipWidth / 2} ${-margin.top + 30} )`)
+
+  const tooltipText = tooltipContent.append("text")
+    .attr("class", "tooltip-content")
+    .style("font-size", "14px")
+    .style("font-weight", 500)
+    .style("fill", textColor)
+
+  const dataYear = data.find(item => item.year === year)
+  formatsInfo.forEach((format, i) => {
+    tooltipText.append("tspan")
+      .attr("class", `sales-${format.id}`)
+      .attr("x", 0)
+      .attr("y", i * textLineHeight)
+      .text(`${format.label}: ${d3.format(",.1r")(dataYear[format.id])}M$`);
+
+    tooltipContent.append("circle")
+      .attr("cx", -10)
+      .attr("cy", i * textLineHeight - 4)
+      .attr("r", 6)
+      .attr("fill", format.color)
+  });
+
 };
 
 const handleMouseEvents = (data) => {
