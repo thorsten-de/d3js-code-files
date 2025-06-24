1195f03d528eee3b4721187c215b54a763f2fc08
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-24 17:18:32 +0200

    Create scales
    - linear scale for length on y axis
    - log scale for Population on x axis
    - radial scale for weight represented as circle area
    - ordinal scale for conservation status as circle color

--- chapter_07/7.4-Refining_data_updates/start/js/scatterplot.js
+++ chapter_07/7.4-Refining_data_updates/start/js/scatterplot.js
@@ -13,4 +13,24 @@ const drawScatterplot = (data) => {
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+  const maxSize = d3.max(data, d => d.max_size_m);
+  yScale = d3.scaleLinear()
+    .domain([0, maxSize])
+    .range([innerHeight, 0])
+    .nice();
+
+  const maxPopulation = d3.max(data, d => d.global_population_estimate)
+  xScale = d3.scaleLog()
+    .domain(([1, maxPopulation]))
+    .range([0, innerWidth])
+    .nice();
+
+  colorScale = d3.scaleOrdinal()
+    .domain(conservationStatuses.map(s => s.id))
+    .range(conservationStatuses.map(s => s.color));
+
+  maxWeight = d3.max(data, d => d.max_wight_t)
+  rScale = d3.scaleRadial()
+    .domain([0, maxWeight])
+    .range([0, 45])
 };
