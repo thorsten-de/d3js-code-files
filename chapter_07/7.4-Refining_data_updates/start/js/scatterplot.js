5aae56712ad7bc6be26f55a89aa82ffd9a0f33be
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-24 17:28:11 +0200

    Draw scatterplot

--- chapter_07/7.4-Refining_data_updates/start/js/scatterplot.js
+++ chapter_07/7.4-Refining_data_updates/start/js/scatterplot.js
@@ -29,8 +29,40 @@ const drawScatterplot = (data) => {
     .domain(conservationStatuses.map(s => s.id))
     .range(conservationStatuses.map(s => s.color));
 
-  maxWeight = d3.max(data, d => d.max_wight_t)
+  maxWeight = d3.max(data, d => d.max_weight_t)
   rScale = d3.scaleRadial()
     .domain([0, maxWeight])
     .range([0, 45])
+
+  innerChart.append("text")
+    .attr("x", innerWidth)
+    .attr("y", innerHeight + 30)
+    .text("Estimated population")
+    .style("text-anchor", "end")
+    .style("dominant-baseline", "hanging")
+
+  var bottomAxis = d3.axisBottom(xScale);
+  innerChart.append("g")
+    .attr("transform", `translate(0, ${innerHeight})`)
+    .call(bottomAxis);
+
+  svg.append("text")
+    .attr("y", 20)
+    .text("Max size (m)")
+
+  var leftAxis = d3.axisLeft(yScale);
+  innerChart.append("g")
+    .call(leftAxis);
+
+  innerChart.selectAll("circle")
+    .data(data)
+    .join("circle")
+    .attr("cx", d => xScale(d.global_population_estimate))
+    .attr("cy", d => yScale(d.max_size_m))
+    .attr("r", d => rScale(d.max_weight_t))
+    .attr("fill", d => colorScale(d.status))
+    .attr("stroke-width", 2)
+    .attr("stroke", d => colorScale(d.status))
+    .attr("fill-opacity", 0.6)
+
 };
