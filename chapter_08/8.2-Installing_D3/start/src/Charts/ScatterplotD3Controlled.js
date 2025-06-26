79af880f4f774800f4c4c56ec12af34dd3f8efa4
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-26 17:59:07 +0200

    Draw the scatterplot with d3 as usual

--- chapter_08/8.2-Installing_D3/start/src/Charts/ScatterplotD3Controlled.js
+++ chapter_08/8.2-Installing_D3/start/src/Charts/ScatterplotD3Controlled.js
@@ -10,11 +10,46 @@ const ScatterplotD3Controlled = props => {
   const innerWidth = width - props.margin.left - props.margin.right;
   const innerHeight = height - props.margin.top - props.margin.bottom;
 
-  const plotRef = useRef();
-  useEffect(() => {
+  const drawScatterPlot = () => {
     const plotContainer = d3.select(plotRef.current);
-    console.log(plotContainer)
-  }, [])
+
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(props.data, d => d.user_count)])
+      .range([0, innerWidth])
+      .nice();
+
+    const bottomAxis = d3.axisBottom(xScale)
+      .ticks([3])
+      .tickFormat(d3.format("d"));
+
+    plotContainer.append("g")
+      .attr("class", "axis")
+      .attr("transform", `translate(0 ${innerHeight})`)
+      .call(bottomAxis);
+
+    const yScale = d3.scaleLinear()
+      .domain([0, 100])
+      .range([innerHeight, 0]);
+
+    const leftAxis = d3.axisLeft(yScale)
+      .ticks([5]);
+
+    plotContainer.append("g")
+      .attr("class", "axis")
+      .call(leftAxis);
+
+    plotContainer.selectAll(".scatterplot-circle")
+      .data(props.data)
+      .join("circle")
+      .attr("class", "scatterplot-circle")
+      .attr("cx", d => xScale(d.user_count))
+      .attr("cy", d => yScale(d.retention_percentage))
+      .attr("r", 6)
+      .attr("fill", d => props.colorScale(d.id))
+  };
+
+  const plotRef = useRef();
+  useEffect(drawScatterPlot, [innerWidth, innerHeight, props])
 
   return (
     <Card>
