4b77901977cca4469b8365cd940b2e0161347940
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-26 17:25:19 +0200

    Use a ref to define a g element that can be controlled by d3

--- chapter_08/8.2-Installing_D3/start/src/Charts/ScatterplotD3Controlled.js
+++ chapter_08/8.2-Installing_D3/start/src/Charts/ScatterplotD3Controlled.js
@@ -1,3 +1,6 @@
+import { useRef, useEffect } from 'react';
+import * as d3 from 'd3';
+
 import Card from '../UI/Card';
 import ChartContainer from '../ChartComponents/ChartContainer';
 
@@ -7,10 +10,14 @@ const ScatterplotD3Controlled = props => {
   const innerWidth = width - props.margin.left - props.margin.right;
   const innerHeight = height - props.margin.top - props.margin.bottom;
 
+  const plotRef = useRef();
+
   return (
     <Card>
       <h2>Retention vs Usage</h2>
-      <ChartContainer width={width} height={height} margin={props.margin}></ChartContainer>
+      <ChartContainer width={width} height={height} margin={props.margin}>
+        <g ref={plotRef}></g>
+      </ChartContainer>
     </Card>
   )
 };
