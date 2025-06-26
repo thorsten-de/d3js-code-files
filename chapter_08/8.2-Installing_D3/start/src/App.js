25e393b0dc27d76af26901fbe586e3cf40477041
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-06-26 16:58:17 +0200

    Loading data into App.js

--- chapter_08/8.2-Installing_D3/start/src/App.js
+++ chapter_08/8.2-Installing_D3/start/src/App.js
@@ -1,14 +1,26 @@
-import { useState } from 'react';
+import * as d3 from 'd3';
+import { useEffect, useState } from 'react';
 
 import Charts from './Charts/Charts';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const dataURL = "https://d3js-in-action-third-edition.github.io/hosted-data/apis/front_end_frameworks.json";
+
+    d3.json(dataURL)
+      .then(data => {
+        setData(data);
+        setLoading(false);
+      })
+  }, []);
 
   return (
     <div className="container">
       {loading && <div className="loading">Loading...</div>}
-      {!loading && <Charts />}
+      {!loading && <Charts data={data} />}
     </div>
   );
 };
