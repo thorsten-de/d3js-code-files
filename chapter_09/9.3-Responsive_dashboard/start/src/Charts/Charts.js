3dda541829aa5703154bc5b2998cdc324f92a65a
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-07-04 19:18:51 +0200

    Reduce information of rankings on mobile devices

--- chapter_09/9.3-Responsive_dashboard/start/src/Charts/Charts.js
+++ chapter_09/9.3-Responsive_dashboard/start/src/Charts/Charts.js
@@ -1,27 +1,79 @@
-import { Fragment } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import * as d3 from 'd3';
 
 import Rankings from './Rankings';
+import RankingsMobile from './RankingsMobile'
 import ScatterplotReactControlled from './ScatterplotReactControlled';
 import BarChart from './BarChart';
 
+const rankingFilters = [
+  { id: "satisfaction", label: "Satisfaction" },
+  { id: "interest", label: "Interest" },
+  { id: "usage", label: "Usage" },
+  { id: "awareness", label: "Awareness" },
+];
+
+
+const breakPoint = 600;
+const getLayout = () => window.innerWidth >= breakPoint ? "desktop" : "mobile";
+
 const Charts = props => {
   const margin = { top: 30, right: 10, bottom: 50, left: 60 };
+  const [layout, setLayout] = useState(getLayout());
+  const [activeFilter, setActiveFilter] = useState("satisfaction");
 
   const colorScale = d3.scaleOrdinal()
     .domain(props.data.ids)
     .range(d3.schemeTableau10);
+
+  const filterSelectionHandler = (id) => {
+    if (activeFilter !== id) {
+      setActiveFilter(id);
+    }
+  };
+
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      const windowWidth = window.innerWidth;
+      if ((windowWidth >= breakPoint && layout === "mobile")
+        || (windowWidth < breakPoint && layout === "desktop")) {
+        setLayout(getLayout());
+      }
+    };
+
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    }
+  }, [layout])
 
   return (
     <Fragment>
       <h1>Front-end Frameworks</h1>
       <div className='row'>
         <div className='col-12 col-lg-9'>
-          <Rankings
-            margin={margin}
-            data={props.data}
-            colorScale={colorScale}
-          />
+          {layout === "desktop" ?
+            <Rankings
+              margin={margin}
+              data={props.data}
+              colorScale={colorScale}
+              rankingFilters={rankingFilters}
+              activeFilter={activeFilter}
+              filterSelectionHandler={filterSelectionHandler}
+            />
+            :
+            <RankingsMobile
+              margin={margin}
+              data={props.data}
+              colorScale={colorScale}
+              rankingFilters={rankingFilters}
+              activeFilter={activeFilter}
+              filterSelectionHandler={filterSelectionHandler}
+            />
+          }
         </div>
         <div className='col-12 col-lg-3'>
           <div className='row'>
--- chapter_09/9.3-Responsive_dashboard/start/src/Charts/RankingsMobile.js
+++ chapter_09/9.3-Responsive_dashboard/start/src/Charts/RankingsMobile.js
@@ -9,12 +9,12 @@ import Label from '../ChartComponents/Label';
 import Badge from '../UI/Badge';
 
 
-const Rankings = props => {
+const RankingsMobile = props => {
 
-  const width = 1000;
+  const width = 300;
   const height = 542;
-  const marginRight = 150;
-  const marginLeft = 110;
+  const marginRight = 120;
+  const marginLeft = 30;
   const innerWidth = width - marginLeft - marginRight;
   const innerHeight = height - props.margin.top - props.margin.bottom;
 
@@ -24,6 +24,26 @@ const Rankings = props => {
   const yScale = d3.scalePoint()
     .domain(d3.range(1, props.data.ids.length + 1))
     .range([0, innerHeight]);
+
+  const allYears = props.data.years;
+  const firstYear = allYears[0];
+  const lastYear = allYears[allYears.length - 1];
+
+  const years = [
+    firstYear,
+    Math.round(d3.median([firstYear, lastYear])),
+    lastYear];
+  console.log(years)
+
+  const mobileData = JSON.parse(JSON.stringify(props.data.experience));
+
+  mobileData.forEach(framework => {
+    framework.awareness = framework.awareness.filter(d => years.includes(d.year))
+    framework.interest = framework.interest.filter(d => years.includes(d.year))
+    framework.satisfaction = framework.satisfaction.filter(d => years.includes(d.year))
+    framework.usage = framework.usage.filter(d => years.includes(d.year))
+
+  });
 
 
   return (
@@ -39,7 +59,7 @@ const Rankings = props => {
         height={height}
         margin={{ top: props.margin.top, right: marginRight, bottom: props.margin.bottom, left: marginLeft }}
       >
-        {props.data.years.map(year => (
+        {years.map(year => (
           <g
             key={`line-year-${year}`}
             className="axis"
@@ -61,7 +81,7 @@ const Rankings = props => {
             </text>
           </g>
         ))}
-        {props.data.experience.map((framework, i) => (
+        {mobileData.map((framework, i) => (
           <g key={`curve-${framework.id}`}>
             <Curve
               data={framework[props.activeFilter]}
@@ -72,15 +92,7 @@ const Rankings = props => {
               stroke={props.colorScale(framework.id)}
               strokeWidth={5}
             />
-            {framework[props.activeFilter][0].rank &&
-              <Label
-                x={-25}
-                y={yScale(framework[props.activeFilter][0].rank)}
-                color={props.colorScale(framework.id)}
-                label={framework.name}
-                textAnchor={"end"}
-              />
-            }
+
             <Label
               x={innerWidth + 25}
               y={yScale(framework[props.activeFilter][framework[props.activeFilter].length - 1].rank)}
@@ -106,4 +118,4 @@ const Rankings = props => {
   )
 };
 
-export default Rankings;
+export default RankingsMobile;
