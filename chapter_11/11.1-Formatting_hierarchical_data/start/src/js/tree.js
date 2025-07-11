0463ba6b3d37e498e5fb3ac94c34a7440e68da66
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-07-11 14:00:16 +0200

    Draw tree links

--- chapter_11/11.1-Formatting_hierarchical_data/start/src/js/tree.js
+++ chapter_11/11.1-Formatting_hierarchical_data/start/src/js/tree.js
@@ -1,5 +1,6 @@
 import { tree } from "d3-hierarchy";
 import { select } from "d3-selection";
+import { link, curveBumpX } from "d3-shape";
 
 export const drawTree = root => {
   const width = 1200;
@@ -8,12 +9,30 @@ export const drawTree = root => {
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
 
+  // x/width and y/height are swapped to produce the tree left-to-right
+
   const treeLayoutGenerator = tree()
-    .size([innerWidth, innerHeight])
+    .size([innerHeight, innerWidth])
+
+  treeLayoutGenerator(root)
 
   const svg = select("#tree")
     .append("svg")
     .attr("viewBox", `0 0 ${width} ${height}`)
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+  const linkGenerator = link(curveBumpX)
+    .x(d => d.y)
+    .y(d => d.x);
+
+
+  svg.selectAll(".tree-link")
+    .data(root.links())
+    .join("path")
+    .attr("class", "tree-link")
+    .attr("d", d => linkGenerator(d))
+    .attr("fill", "none")
+    .attr("stroke", "gray")
+    .attr("stroke-opacity", 0.6);
 }
