6863d895b975f761e26eb3e2d61d93e24b857a70
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-07-15 19:20:18 +0200

    Generate matrix labels

--- chapter_12/12.2-Adjacency_matrix/start/src/js/matrix.js
+++ chapter_12/12.2-Adjacency_matrix/start/src/js/matrix.js
@@ -1,5 +1,5 @@
 import { sort, max } from "d3-array";
-import { select } from "d3-selection";
+import { select, selectAll } from "d3-selection";
 import { opacityScale, opacityScale } from "./scales";
 
 export const drawMatrix = (nodes, edges) => {
@@ -19,6 +19,7 @@ export const drawMatrix = (nodes, edges) => {
     return acc;
   }, {});
 
+  const margin = { top: 100, right: 0, bottom: 0, left: 100 }
   const squareWidth = 16;
   const padding = 2;
   const size = nodes.length * (squareWidth + padding);
@@ -50,15 +51,43 @@ export const drawMatrix = (nodes, edges) => {
 
   const svg = select("#matrix")
     .append("svg")
-    .attr("viewBox", `0 0 ${size} ${size}`)
+    .attr("viewBox", `0 0 ${size + margin.left} ${size + margin.top}`)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
-  svg.selectAll("rect")
+  svg.selectAll(".grid-square")
     .data(matrix)
     .join("rect")
+    .attr("class", "grid-square")
     .attr("x", d => d.x)
     .attr("y", d => d.y)
     .attr("width", squareWidth)
     .attr("height", squareWidth)
     .attr("fill", "#364652")
     .attr("opacity", d => sceneScale(d.weight))
+
+  svg.selectAll(".character-label")
+    .data(nodes)
+    .join("text")
+    .attr("class", d => `matrix-label label-${d.id}`)
+    .attr("y", (d, i) => (i + 0.5) * (squareWidth + padding))
+    .attr("x", -5)
+    .attr("dominant-baseline", "middle")
+    .attr("text-anchor", "end")
+    .style("font-size", "12px")
+    .text(d => d.name)
+
+
+  svg.selectAll(".character-label")
+    .data(nodes)
+    .join("text")
+    .attr("class", d => `matrix-label label-${d.id}`)
+    .attr("transform", "rotate(-90)")
+    .attr("y", (d, i) => (i + 0.5) * (squareWidth + padding))
+    .attr("x", 5)
+    .attr("dominant-baseline", "middle")
+    .attr("text-anchor", "start")
+    .style("font-size", "12px")
+    .text(d => d.name)
+
 };
