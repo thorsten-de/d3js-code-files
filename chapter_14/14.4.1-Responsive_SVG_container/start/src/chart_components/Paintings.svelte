c8aa55ed716cc8ac517c7c0cfcfeaa1a3a3c9679
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-07-23 16:25:46 +0200

    Use force directed layout to cluster paintings per month

--- chapter_14/14.4.1-Responsive_SVG_container/start/src/chart_components/Paintings.svelte
+++ chapter_14/14.4.1-Responsive_SVG_container/start/src/chart_components/Paintings.svelte
@@ -1,15 +1,46 @@
 <script>
+  import { forceCollide, forceSimulation, forceX, forceY } from "d3-force";
+
   export let paintingAreaScale;
   export let paintingDefaultRadius;
   export let paintings;
   export let monthScale;
   export let radius;
+
+  let simulation = forceSimulation(paintings);
+  let nodes = [];
+
+  simulation.on("tick", () => {
+    nodes = simulation.nodes();
+  });
+
+  $: {
+    simulation
+      .force(
+        "x",
+        forceX((d) => (d.month !== "" ? radius * Math.sin(monthScale(d.month)) : 0)).strength(0.5)
+      )
+      .force(
+        "y",
+        forceY((d) => (d.month !== "" ? radius * -Math.cos(monthScale(d.month)) : 0)).strength(0.5)
+      )
+      .force(
+        "collide",
+        forceCollide().radius((d) =>
+          d.width_cm === null || d.height_cm === null
+            ? paintingDefaultRadius + 1
+            : paintingAreaScale(d.area_cm2) + 1
+        )
+      )
+      .alpha(0.5)
+      .alphaDecay(0.1);
+  }
 </script>
 
-{#each paintings as painting}
+{#each nodes as node}
   <circle
-    cx={painting.month !== "" ? radius * Math.sin(monthScale(painting.month)) : 0}
-    cy={painting.month !== "" ? radius * -Math.cos(monthScale(painting.month)) : 0}
-    r={painting.area_cm2 ? paintingAreaScale(painting.area_cm2) : paintingDefaultRadius}
+    cx={node.x}
+    cy={node.y}
+    r={node.area_cm2 ? paintingAreaScale(node.area_cm2) : paintingDefaultRadius}
   />
 {/each}
