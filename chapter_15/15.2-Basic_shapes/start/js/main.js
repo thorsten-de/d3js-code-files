0451b70e2d4228dfb807a624bf893a652fd93295
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-07-28 18:12:01 +0200

    Set adaptable canvas dimensions

--- chapter_15/15.2-Basic_shapes/start/js/main.js
+++ chapter_15/15.2-Basic_shapes/start/js/main.js
@@ -2,3 +2,12 @@ const canvas = d3.select("#canvas")
   .append("canvas")
   .style("border", "1px solid black")
 
+const container = document.querySelector("#canvas");
+const width = container.offsetWidth;
+const height = width / 3;
+
+canvas
+  .attr("width", width)
+  .attr("height", height);
+
+
