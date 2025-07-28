85886440555eb897671d383850d3b21a8dd35c3b
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-07-28 18:21:54 +0200

    Draw rectangles

--- chapter_15/15.2-Basic_shapes/start/js/main.js
+++ chapter_15/15.2-Basic_shapes/start/js/main.js
@@ -21,3 +21,14 @@ context.beginPath();
 context.moveTo(66, 60)
 context.lineTo(186, 300);
 context.stroke();
+
+// draw rectangles
+context.rect(346, 33, 160, 80);
+context.roundRect(346, 133, 160, 80, [20]);
+context.fillStyle = "#6ba5d7";
+context.fill();
+
+context.beginPath();
+context.rect(346, 233, 80, 80);
+context.strokeStyle = "#6ba5d7"
+context.stroke();
