d2c3bf16940e8401cc803f3266810ea28677acf5
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-07-28 18:14:33 +0200

    Use devicePixelRatio for HighDpi devices

--- chapter_15/15.2-Basic_shapes/start/js/main.js
+++ chapter_15/15.2-Basic_shapes/start/js/main.js
@@ -1,13 +1,15 @@
 const canvas = d3.select("#canvas")
   .append("canvas")
   .style("border", "1px solid black")
+  .style("max-width", "100%")
 
 const container = document.querySelector("#canvas");
+const devicePixelRatio = window.devicePixelRatio;
 const width = container.offsetWidth;
 const height = width / 3;
 
 canvas
-  .attr("width", width)
-  .attr("height", height);
+  .attr("width", width * devicePixelRatio)
+  .attr("height", height * devicePixelRatio);
 
 
